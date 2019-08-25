df7dba39c9843255db49e72ffc59f07d4f49473e
Dereck Tu <dereck.tu@hotmail.com>
2019-08-25 07:22:47 -0400

    Fix bugs

--- omnical-discord/src/main/java/AudioReceiver.java
+++ omnical-discord/src/main/java/AudioReceiver.java
@@ -5,30 +5,33 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class AudioReceiver implements AudioReceiveHandler {
-    List<FakeByteArray> byteList = new ArrayList<>();
-    String userId = null;
+    Map<String, List<FakeByteArray>> byteLists = new HashMap<>();
 
     public boolean canReceiveUser() {
         return true;
     }
 
     public void handleUserAudio(UserAudio userAudio) {
-        userId = Util.customID(userAudio.getUser());
-        byteList.add(new FakeByteArray(userAudio.getAudioData(1.0)));
+        String userId = Util.customID(userAudio.getUser());
+        if(byteLists.get(userId) == null)
+            byteLists.put(userId, new ArrayList<FakeByteArray>());
+        byteLists.get(userId).add(new FakeByteArray(userAudio.getAudioData(1.0)));
 
     }
 
-    public void writeFile() {
-        if (userId != null) {
+    public void writeFiles() {
+        byteLists.forEach((userId, byteList) -> {
             try (FileOutputStream s = new FileOutputStream(new File("audio" + "/" + userId + ".txt"))) {
                 for (FakeByteArray arr : byteList)
                     s.write(arr.getArr());
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        });
     }
 }
--- omnical-discord/src/main/java/Main.java
+++ omnical-discord/src/main/java/Main.java
@@ -14,6 +14,7 @@ import javax.security.auth.login.LoginException;
 import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 public class Main extends ListenerAdapter {
@@ -23,6 +24,8 @@ public class Main extends ListenerAdapter {
 
 
     public static void main(String[] args) throws LoginException {
+        for(File f : new File("audio_out").listFiles())
+            f.delete();
         JDABuilder builder = new JDABuilder(AccountType.BOT)
                 .setToken(Util.getDiscKey())
                 .addEventListeners(new Main());
@@ -61,12 +64,12 @@ public class Main extends ListenerAdapter {
                 }
                 audioManager.closeAudioConnection();
 
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                receiver.writeFile();
+//                try {
+//                    Thread.sleep(20);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+                receiver.writeFiles();
                 File[] files = new File("audio").listFiles();
                 for (File f : files) {
                     try {
@@ -84,7 +87,6 @@ public class Main extends ListenerAdapter {
                                 "audio_out" + "/" + baseName + ".wav").start();
                         p.waitFor();
                         f.delete();
-                        sendToNLPServer(new File("audio_out/" + baseName + ".wav"));
                     } catch (IOException | InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -92,6 +94,7 @@ public class Main extends ListenerAdapter {
                     audioManager.setReceivingHandler(null);
                     audioManager.setSendingHandler(null);
                 }
+                sendToNLPServer(new File("audio_out/"));
             } else if (event.getMessage().getContentRaw().toLowerCase().startsWith("!test ")) {
                 sendToNLPServer(new File(event.getMessage().getContentRaw().substring("!test ".length())));
             }
@@ -100,35 +103,71 @@ public class Main extends ListenerAdapter {
         }
     }
 
-    private void sendToNLPServer(File audioFile) {
+    private void sendToNLPServer(File audioFolder) {
         SpeechConfig speechConfig = SpeechConfig.fromSubscription(Util.getAzureKey(), "westus");
-        AudioConfig audioConfig = AudioConfig.fromWavFileInput(audioFile.getPath());
-        SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
-        speechRecognizer.recognized.addEventListener((o, e) -> {
-            if (e.getResult().getReason() == ResultReason.RecognizedSpeech && !e.getResult().getText().isEmpty()) {
-                System.out.print("RECOGNIZED: ");
-                System.out.println(e.getResult().getText());
-                String url = "https://omnical-ai.azurewebsites.net/api/analyze" +
-                        "?code=dCB9FpHWVhqwFYaxpJc5xq2cuQoBU1t9O30JS2SrPZL5iN/yAYw6QQ==" +
-                        "&discordID=" + URLEncoder.encode(audioFile.getName().substring(audioFile.getName().indexOf('-') + 1, audioFile.getName().length() - 4)) +
-                        "&text=" + URLEncoder.encode(e.getResult().getText()) +
-                        "&from=" + URLEncoder.encode(audioFile.getName().substring(0, audioFile.getName().indexOf('-')));
-                String out = Util.executeGet(url);
-                System.out.println("STT API RESULT: " + out);
-            } else if (e.getResult().getReason() == ResultReason.NoMatch) {
-                System.out.println("NO MATCH");
+        File[] audioFiles = audioFolder.listFiles();
+        String[] textIDs = new String[audioFiles.length];
+        String[] numberIDs = new String[audioFiles.length];
+        String[] textContents = new String[audioFiles.length];
+        boolean[] textContentFillingDones = new boolean[audioFiles.length];
+        for(int i = 0; i < textContentFillingDones.length; i++)
+            textContentFillingDones[i] = false;
+        // filling txtids and nbrids
+        for(int i = 0; i < audioFiles.length; i++) {
+            String[] names = audioFiles[i].getName().substring(0, audioFiles[i].getName().length() - 4).split("-");
+            textIDs[i] = names[0];
+            numberIDs[i] = names[1];
+        }
+        // filling textContents
+        for(int i = 0; i < audioFiles.length; i++) {
+            final int I = i;
+            File audioFile = audioFiles[i];
+            AudioConfig audioConfig = AudioConfig.fromWavFileInput(audioFile.getPath());
+            SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+            speechRecognizer.recognized.addEventListener((o, e) -> {
+                if (e.getResult().getReason() == ResultReason.RecognizedSpeech && !e.getResult().getText().isEmpty()) {
+                    System.out.print("RECOGNIZED: ");
+                    System.out.println(e.getResult().getText());
+                    textContents[I] = e.getResult().getText();
+                } else if (e.getResult().getReason() == ResultReason.NoMatch) {
+                    System.out.println("NO MATCH");
+                }
+            });
+            speechRecognizer.sessionStopped.addEventListener((o, e) -> {
+                System.out.print("DONE: ");
+                System.out.println(e);
+                textContentFillingDones[I] = true;
+            });
+            try {
+                speechRecognizer.startContinuousRecognitionAsync().get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
             }
-        });
-        speechRecognizer.sessionStopped.addEventListener((o, e) -> {
-            System.out.print("DONE: ");
-            System.out.println(e);
-        });
-        try {
-            speechRecognizer.startContinuousRecognitionAsync().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        }
+
+        outer:
+        while(true) {
+            for(boolean b : textContentFillingDones) {
+                if(b == false)
+                    continue outer;
+            }
+            break;
+        }
+
+        for(int i = 0; i < textIDs.length; i++) {
+            for(int j = 0; j < numberIDs.length; j++) {
+                if(i != j) {
+                    String url = "https://omnical-ai.azurewebsites.net/api/analyze" +
+                            "?code=dCB9FpHWVhqwFYaxpJc5xq2cuQoBU1t9O30JS2SrPZL5iN/yAYw6QQ==" +
+                            "&discordID=" + URLEncoder.encode(numberIDs[j]) +
+                            "&text=" + URLEncoder.encode(textContents[i]) +
+                            "&from=" + URLEncoder.encode(textIDs[i]);
+                    String out = Util.executeGet(url);
+                    System.out.println("STT API RESULT: " + out);
+                }
+            }
         }
     }
 }
